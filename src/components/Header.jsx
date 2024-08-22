17369a6d78dcb580067a5cf4fee39aa93203ca63
hazimaiman <hazimaiman.azman@gmail.com>
2024-08-22 11:57:33 +0800

    Initial commit for frontend

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import NoteAltIcon from '@mui/icons-material/NoteAlt';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <NoteAltIcon/>
+        myNote
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
